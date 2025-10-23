c4c5f48c50675f80e62ad6f1d83fc59c43f751b3
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-10-23 09:07:00 +0700

    feat: setup prisma service and validation service using zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule],
 })
 export class AppModule {}
---
+++ src/common/prisma.service.ts
@@ -0,0 +1,46 @@
+import { Logger } from "winston";
+import { PrismaClient } from "@prisma/client/extension";
+import { Prisma } from "generated/prisma";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
+
+@Injectable()
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, string> implements OnModuleInit {
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {
+    super({
+      log: [
+        {
+          emit: "event",
+          level: "info"
+        },
+        {
+          emit: "event",
+          level: "warn"
+        },
+        {
+          emit: "event",
+          level: "error"
+        },
+        {
+          emit: "event",
+          level: "query"
+        }
+      ]
+    })
+  }
+
+  onModuleInit() {
+    this.$on('info', (e) => {
+      this.logger.info(e)
+    })
+    this.$on('warn', (e) => {
+      this.logger.warn(e)
+    })
+    this.$on('error', (e) => {
+      this.logger.error(e)
+    })
+    this.$on('query', (e) => {
+      this.logger.info(e)
+    })
+  }
+}
