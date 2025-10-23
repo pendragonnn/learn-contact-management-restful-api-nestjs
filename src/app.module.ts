682d0945b312480151a3a4f7f6a4ce4083343040
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-10-23 10:59:50 +0700

    feat[user]: add user register API and initiate unit testing for user feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UserModule],
 })
 export class AppModule {}
